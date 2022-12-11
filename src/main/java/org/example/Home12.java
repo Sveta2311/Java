152fc8877d45ec3769dad08d37def84b0ede0227
Sveta <margaritka2132@mail.ru>
2022-12-11 20:44:33 +0400

    added new files

--- src/main/java/org/example/Home12.java
+++ src/main/java/org/example/Home12.java
@@ -8,7 +8,9 @@ import java.util.Scanner;
 public class Home12 {
     public static void main(String[] args) {
         System.out.print(palindrome());
-    }public static boolean palindrome() {
+
+    }
+    public static boolean palindrome() {
         System.out.print("Введите строку: ");
         Scanner scanner = new Scanner(System.in);
         String str1 = scanner.nextLine();
